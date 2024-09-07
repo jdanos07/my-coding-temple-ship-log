4eebd6bf7dd94b76bf3fdb748d9986c66129f535
jdanos07 <jdanos07@gmail.com>
2024-09-06 20:50:46 -0400

    changed python code

--- adventure_log.py
+++ adventure_log.py
@@ -1,5 +1,6 @@
 def greet_coding_temple():
-    print("Hello, Coding Temple Navigators!")
+    print("Hello, Coding Temple Navigators!?")
+    
 
 if __name__ == "__main__":
     greet_coding_temple()    
